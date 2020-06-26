1e23418ba37aa6ac646e791350e946645a2f1a5c
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-26 23:53:37 +0200

    update init example

--- lecture3/printf/__init__.py
+++ lecture3/printf/__init__.py
@@ -10,7 +10,7 @@ def exists():
 @check50.check(exists)
 def compiles():
     """hello.c compiles."""
-    check50.c.compile("hello.c", lcs50=True)
+    check50.c.compile("hello.c", lcs50=False)
 
 @check50.check(compiles)
 def veronica():
