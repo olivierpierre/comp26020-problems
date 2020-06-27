525d047ec9eaeca32c8b37d45021183d48ab1453
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-27 10:44:19 +0200

    add another test for lecture3

---
+++ lecture3/printf2/__init__.py
@@ -0,0 +1,14 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    check50.exists("printf2.c")
+
+@check50.check(exists)
+def compiles():
+    check50.c.compile("printf2.c", cc="gcc")
+
+@check50.check(compiles)
+def goodbye_printed():
+    check50.run("./printf2").stdout("This should work!").exit()
