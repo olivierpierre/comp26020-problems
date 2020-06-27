bc5ce662d1d12b0e8b2aba3a77899baaf581e765
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-27 15:33:43 +0200

    update variable test

--- lecture04/variables/__init__.py
+++ lecture04/variables/__init__.py
@@ -4,17 +4,26 @@ import check50.c
 @check50.check()
 def exists():
     check50.exists("variable.c")
+    with open("variable.c") as f:
+        buf = f.read()
+    return buf
 
 @check50.check(exists)
 def compiles():
     check50.c.compile("variable.c", cc="gcc")
 
 @check50.check(exists)
-def has_int_var():
-    with open("variable.c") as f:
-        buf = f.read()
+def has_variables():
+#    with open("variable.c") as f:
+#        buf = f.read()
     if "int_var" not in buf:
-        raise check50.Failure("Could not find mention to int_var")
+        raise check50.Failure("Could not find mention of int_var in the source")
+    if "double_var" not in buf:
+        raise check50.Failure("Could not find mention of dobule_var in the source")
+
+@check50.check(exists)
+def has_format_specifiers():
+    with op
 
 @check50.check(compiles)
 def output_correct():
