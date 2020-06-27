c3a0b70da7aa8b5042213ee3a972ac182a801973
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-27 15:55:28 +0200

    finalize variable exercize

--- lecture04/variables/__init__.py
+++ lecture04/variables/__init__.py
@@ -25,12 +25,12 @@ def has_variables(sources_buf):
 def has_format_specifier(sources_buf):
     buf = sources_buf
     if "%d" not in buf and "%i" not in buf:
-        raise check50.Failure("Could not find and integer format specifier "
+        raise check50.Failure("Could not find an integer format specifier "
                 "(%d or %i)")
     if "%f" not in buf and "%F" not in buf and "%q" not in buf and \
             "%G" not in buf:
-        raise check50.Failure("Could not find and integer format specifier "
-                "(%f, %F, %q, %G)")
+        raise check50.Failure("Could not find a floating point format "
+                "specifier "(%f, %F, %q, %G)")
 
 @check50.check(compiles)
 def output_correct():
