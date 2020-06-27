6ece2e3ebb75722152fe0d85ac3ba7cec16af428
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-27 15:22:15 +0200

    update variable test

--- lecture04/variables/__init__.py
+++ lecture04/variables/__init__.py
@@ -9,8 +9,15 @@ def exists():
 def compiles():
     check50.c.compile("variable.c", cc="gcc")
 
+@check50.check(exists)
+def has_int_var():
+    with open("variable.c") as f:
+        buf = f.read()
+    if "int_var" not in buf:
+        raise check50.Failure("Could not find mention to int_var")
+
 @check50.check(compiles)
-def goodbye_printed():
+def output_correct():
     check50.run("./variable").stdout("int_var: [0-9]+", regex=True)\
             .stdout("double_var: [0-9]+\.[0-9]+", regex=True)\
             .exit()
