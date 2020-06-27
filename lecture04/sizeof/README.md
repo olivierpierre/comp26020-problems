d7f33698fd2b9cba190a710aac99fd859855a2be
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-27 16:33:49 +0200

    size problem

---
+++ lecture04/sizeof/README.md
@@ -0,0 +1,22 @@
+# Lecture 4 Problem 1
+
+Write a simple program printing the size of `int` variables, followed on the
+next line by the size of `double` variables, followed on a third line by the
+size of `unsigned long long` variables. On a last line, print the value of
+the multiplication of these 3 sizes. The expected output (on a modern 64 bits
+machine) is:
+
+```
+4
+8
+8
+256
+```
+
+To check the correctness of your program, use CS50 [sandbox](sandbox.cs50.io)
+or [IDE](ide.cs50.io) and write it in a file named `goodbye.c`. In a terminal,
+with that file in the local directory, check with this command:
+
+```shell
+check50 -l --log olivierpierre/comp26020-problems/master/lecture04/sizeof
+```
---
+++ lecture04/sizeof/__init__.py
@@ -0,0 +1,35 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    check50.exists("sizes.c")
+    with open("sizes.c") as f:
+        sources_buf = f.read()
+    return sources_buf
+
+@check50.check(exists)
+def compiles():
+    check50.c.compile("sizes.c", cc="gcc")
+
+@check50.check(exists)
+def has_sizeof(sources_buf):
+    soi_re = "sizeof\(\s*int\s*\)"
+    sod_re = "sizeof\(\s*double\s*\)"
+    soull_re = "sizeof\(\s*unsigned\s*long\s*long\s*\)"
+
+    if not re.search(soi_re, sources_buf):
+        raise check50.Failure("Could not find a call to sizeof(int)")
+    if not re.search(sod_re, sources_buf):
+        raise check50.Failure("Could not find a call to sizeof(double)")
+    if not re.search(soull_re, sources_buf):
+        raise check50.Failure("Could not find a call to sizeof(unsigned long "
+                "long)")
+
+@check50.check(compiles)
+def output_correct():
+    check50.run("./sizes").stdout("4")\
+            .stdout("8")\
+            .stdout("8")\
+            .stdout("256")\
+            .exit()
