2e7f2703f06a82bda350fcccc459115b27e1a25f
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-27 10:22:55 +0200

    update printf test

--- lecture3/printf/__init__.py
+++ lecture3/printf/__init__.py
@@ -1,18 +1,14 @@
- 
 import check50
 import check50.c
 
 @check50.check()
 def exists():
-    """hello.c exists."""
-    check50.exists("hello.c")
+    check50.exists("goodbye.c")
 
 @check50.check(exists)
 def compiles():
-    """hello.c compiles."""
-    check50.c.compile("hello.c", lcs50=False)
+    check50.c.compile("goodbye.c", cc="gcc", cflags="-ggdb")
 
 @check50.check(compiles)
 def goodbye_printed():
-    """prints 'Goodbye, world!' """
-    check50.run("./hello").stdout("Goodbye, world!").exit()
+    check50.run("./goodbye").stdout("[Gg]oodbye, [Ww]orld!", regex=True).exit()
