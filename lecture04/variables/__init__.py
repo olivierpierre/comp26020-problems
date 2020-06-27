1aeea36a12821e7d736a63440c709aa46c245842
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-27 15:38:00 +0200

    update variable test

--- lecture04/variables/__init__.py
+++ lecture04/variables/__init__.py
@@ -21,10 +21,6 @@ def has_variables():
     if "double_var" not in buf:
         raise check50.Failure("Could not find mention of dobule_var in the source")
 
-@check50.check(exists)
-def has_format_specifiers():
-    with op
-
 @check50.check(compiles)
 def output_correct():
     check50.run("./variable").stdout("int_var: [0-9]+", regex=True)\
