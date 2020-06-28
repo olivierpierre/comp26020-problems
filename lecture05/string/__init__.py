1d8d1d16e0911b53e7c558a24dae20e38f548dc0
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-28 18:48:32 +0200

    x

--- lecture05/string/__init__.py
+++ lecture05/string/__init__.py
@@ -21,12 +21,12 @@ def has_right_size(sources_buf):
     if not res:
         raise check50.Failure("Cannot determine the string size")
 
-    size = int(res.groups[0])
+    size = int(res.groups()[0])
     if size < 9:
         raise check50.Failure("String size not large enough")
 
 @check50.check(exists)
-def has_termination_chracter():
+def has_termination_chracter(sources_buf):
     string_re = "string\[9\]\s*=\s*'\\0'"
     if not re.search(string_re, sources_buf):
         raise check50.failure("Cannot find string termination character")
