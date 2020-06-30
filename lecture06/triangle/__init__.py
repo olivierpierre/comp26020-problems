01b1ba50e9e74badfb75ef2f83c126891f51fc50
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-30 10:31:24 +0200

    x

--- lecture06/triangle/__init__.py
+++ lecture06/triangle/__init__.py
@@ -16,7 +16,7 @@ def compiles():
 @check50.check(compiles)
 def output_correct():
     check50.run("./triangle 0").stdout("").exit()
-#    check50.run("./triangle 1").stdout("*").exit()
+    check50.run("./triangle 1").stdout("*").exit()
 #    check50.run("./triangle 2").stdout("*").stdout("**").exit()
 #    check50.run("./triangle 5")\
 #            .stdout("*")\
