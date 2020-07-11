66774dcbce02ceb46346bdcbf8d89e064fab8845
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-07-11 11:47:03 +0200

    x

---
+++ lecture10/memcpy/__init__.py
@@ -0,0 +1,33 @@
+import check50
+import check50.c
+import re
+
+@check50.check()
+def exists():
+    check50.exists("memcpy.c")
+    with open("memcpy.c") as f:
+        sources_buf = f.read()
+    return sources_buf
+
+@check50.check(exists)
+def compiles():
+    check50.c.compile("memcpy.c", cc="gcc")
+
+@check50.check(compiles)
+def output_correct():
+    check50.run("./memcpy 10")\
+            .stdout("array1:( [0-9]+){10}", regexp=True)\
+            .stdout("array2:( [0-9]+){10}", regexp=True)\
+            .exit()
+    check50.run("./memcpy 20")\
+            .stdout("array1:( [0-9]+){20}", regexp=True)\
+            .stdout("array2:( [0-9]+){20}", regexp=True)\
+            .exit()
+
+@check50.check(compiles)
+def valgrind_memcheck():
+    check50.c.valgrind("./memcpy 20")\
+            .stdout("array1:( [0-9]+){20}", regexp=True)\
+            .stdout("array2:( [0-9]+){20}", regexp=True)\
+            .exit()
+
