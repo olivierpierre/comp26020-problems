d5ede2994f2e9f3f779a863a6df3d28a9077ca1b
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-07-11 12:14:07 +0200

    x

---
+++ lecture10/math/README.md
@@ -0,0 +1,28 @@
+# Lecture 10 Problem 4
+
+Write a C program reading a `double` with `scanf` and asking the user if he
+wants this number to be floored or ceiled. Next, the program performs the 
+requested operation and displays the result. Output examples:
+
+```shell
+./math
+Input a number:
+12.4
+Input 0 for ceil, 1 for floor
+0
+13.000000
+
+./math
+Input a number:
+45.87
+Input 0 for ceil, 1 for floor
+1
+45.000000
+```
+
+To check the correctness of your program, use CS50 [sandbox](sandbox.cs50.io)
+or [IDE](ide.cs50.io) and write it in a file named `math.c`. In a terminal,
+with that file in the local directory, check with this command:
+```shell
+check50 -l --log olivierpierre/comp26020-problems/master/lecture10/math
+```
---
+++ lecture10/math/__init__.py
@@ -0,0 +1,32 @@
+import check50
+import check50.c
+import re
+
+@check50.check()
+def exists():
+    check50.exists("math.c")
+    with open("math.c") as f:
+        sources_buf = f.read()
+    return sources_buf
+
+@check50.check(exists)
+def compiles():
+    check50.c.compile("math.c", cc="gcc")
+
+@check50.check(compiles)
+def output_correct():
+    check50.run("./math")\
+            .stdout("Input a number:")\
+            .stdin("12.4")\
+            .stdout("Input 0 for ceil, 1 for floor")\
+            .stdin("0")\
+            .stdout("12.000000")\
+            .exit()
+    check50.run("./math")\
+            .stdout("Input a number:")\
+            .stdin("442.8")\
+            .stdout("Input 0 for ceil, 1 for floor")\
+            .stdin("1")\
+            .stdout("422.000000")\
+            .exit()
+
