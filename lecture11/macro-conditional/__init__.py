63880edea7aa3f95f13ca0032a762d8e2321429c
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-07-20 15:54:01 +0200

    x

--- lecture11/macro-conditional/__init__.py
+++ lecture11/macro-conditional/__init__.py
@@ -17,25 +17,25 @@ def compiles():
 @check50.check(compiles)
 def output_correct():
     check50.run("./debug")\
-            .stdout("[DEBUG] Allocating memory")\
-            .stdout("[DEBUG] Allocation successfull")\
-            .stdout("[DEBUG] Filling array")\
-            .stdout("[DEBUG] Generated 1000 random numbers")\
-            .stdout("[DEBUG] Dividing numbers into bins")\
-            .stdout("[DEBUG] Printing results")\
-            .stdout("bin 0: [000 - 020[ \**")\
-            .stdout("bin 1: [020 - 040[ \**")\
-            .stdout("bin 2: [040 - 060[ \**")\
-            .stdout("bin 3: [060 - 080[ \**")\
-            .stdout("bin 4: [080 - 100[ \**")\
-            .stdout("[DEBUG] Printing done")\
-            .stdout("[DEBUG] Memory freed")\
+            .stdout("\[DEBUG\] Allocating memory")\
+            .stdout("\[DEBUG\] Allocation successfull")\
+            .stdout("\[DEBUG\] Filling array")\
+            .stdout("\[DEBUG\] Generated 1000 random numbers")\
+            .stdout("\[DEBUG\] Dividing numbers into bins")\
+            .stdout("\[DEBUG\] Printing results")\
+            .stdout("bin 0: \[000 - 020\[ \**")\
+            .stdout("bin 1: \[020 - 040\[ \**")\
+            .stdout("bin 2: \[040 - 060\[ \**")\
+            .stdout("bin 3: \[060 - 080\[ \**")\
+            .stdout("bin 4: \[080 - 100\[ \**")\
+            .stdout("\[DEBUG\] Printing done")\
+            .stdout("\[DEBUG\] Memory freed")\
             .exit()
     check50.run("./release")\
-            .stdout("bin 0: [000 - 020[ \**")\
-            .stdout("bin 1: [020 - 040[ \**")\
-            .stdout("bin 2: [040 - 060[ \**")\
-            .stdout("bin 3: [060 - 080[ \**")\
-            .stdout("bin 4: [080 - 100[ \**")\
+            .stdout("bin 0: \[000 - 020\[ \**")\
+            .stdout("bin 1: \[020 - 040\[ \**")\
+            .stdout("bin 2: \[040 - 060\[ \**")\
+            .stdout("bin 3: \[060 - 080\[ \**")\
+            .stdout("bin 4: \[080 - 100\[ \**")\
             .exit()
 
