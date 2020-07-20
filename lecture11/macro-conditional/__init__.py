0467322148d4b193546d50ecb39c70603c168432
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-07-20 15:49:21 +0200

    x

--- lecture11/macro-conditional/__init__.py
+++ lecture11/macro-conditional/__init__.py
@@ -11,7 +11,7 @@ def exists():
 
 @check50.check(exists)
 def compiles():
-    check50.c.compile("macro-conditional.c", exe_name="debug", cc="gcc", -DDEBUG_MODE="")
+    check50.c.compile("macro-conditional.c", exe_name="debug", cc="gcc", DDEBUG_MODE="")
     check50.c.compile("macro-conditional.c", exe_name = "release", cc="gcc")
 
 @check50.check(compiles)
