a2af9e8bd5bd51f74fb8b1f4cff425f9ab53822c
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-08-27 15:29:54 +0200

    14

--- lecture14/constructor/__init__.py
+++ lecture14/constructor/__init__.py
@@ -15,7 +15,7 @@ def compiles():
 
 @check50.check(exists)
 def validate(sources_buf):
-    if re.search("p\.x\s*=\s*[0-9]+\s*;", sources_buf) or
+    if re.search("p\.x\s*=\s*[0-9]+\s*;", sources_buf) or \
             re.search("p\.y\s*=\s*[0-9]+\s*;", sources_buf):
         raise check50.Failure("Should not initialize p's members in main")
     if "Pair p(10, 20);" not in sources_buf:
