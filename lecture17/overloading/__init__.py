87f54f85df993c2ad1a271b9bfe5ef0250316061
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-08-29 21:10:46 +0200

    17

--- lecture17/overloading/__init__.py
+++ lecture17/overloading/__init__.py
@@ -20,8 +20,8 @@ def validate(sources_buf):
 @check50.check(compiles)
 def output_correct():
     check50.run("./overloading")\
-        .stdout("\(4+5i\) + \(9+11i\) = \(13+16i\)")\
-        .stdout("4 + 9 = 13")\
-        .stdout("5.800000 + 11.200000 = 17.000000")\
+        .stdout("(4\+5i) \+ (9\+11i) = (13\+16i)")\
+        .stdout("4 \+ 9 = 13")\
+        .stdout("5.800000 \+ 11.200000 = 17.000000")\
         .exit()
 
