6f0217911bf990e227f2df04606527cb9acf0020
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2022-08-26 18:21:39 +0100

    x

--- week3-c-pointers-stdlib/16-strtol/__init__.py
+++ week3-c-pointers-stdlib/16-strtol/__init__.py
@@ -14,31 +14,39 @@ def compiles():
     check50.c.compile("strtol.c", cc="gcc")
 
 @check50.check(compiles)
-def output_correct():
+def output_correct_standard_integer():
     check50.run("./strtol")\
             .stdout("please enter an integer number \(base 10\):")\
             .stdin("42\n")\
             .stdout("you have entered: 42")\
             .exit()
 
+@check50.check(compiles)
+def output_correct_negative_integer():
     check50.run("./strtol")\
             .stdout("please enter an integer number \(base 10\):")\
             .stdin("-42\n")\
             .stdout("you have entered: -42")\
             .exit()
 
+@check50.check(compiles)
+def output_correct_letters():
     check50.run("./strtol")\
             .stdout("please enter an integer number \(base 10\):")\
             .stdin("bar\n")\
             .stdout("invalid string")\
             .exit()
 
+@check50.check(compiles)
+def output_correct_overflow():
     check50.run("./strtol")\
             .stdout("please enter an integer number \(base 10\):")\
             .stdin("100000000000000000000\n")\
             .stdout("under/overflow")\
             .exit()
 
+@check50.check(compiles)
+def output_correct_underflow():
     check50.run("./strtol")\
             .stdout("please enter an integer number \(base 10\):")\
             .stdin("-100000000000000000000\n")\
